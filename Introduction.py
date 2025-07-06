c67e9765a6cf03a8fc9e40a9dd3ad0711a2e71e1
TheNameLess001 <bounoirsaifeddine1@gmail.com>
2025-07-06 23:01:31 +0100

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -70,6 +70,40 @@ if not st.session_state["logged"]:
     show_login()
     st.stop()
 else:
+    import os
+
+def change_password(user, db_path=USERS_DB):
+    st.subheader("🔒 Changer mon mot de passe")
+    with st.form("change_pwd_form", clear_on_submit=True):
+        old_pwd = st.text_input("Ancien mot de passe", type="password")
+        new_pwd1 = st.text_input("Nouveau mot de passe", type="password")
+        new_pwd2 = st.text_input("Confirmer le nouveau mot de passe", type="password")
+        submit = st.form_submit_button("Valider")
+    if submit:
+        # Recharge BDD
+        df = pd.read_csv(db_path, dtype=str)
+        user_row = (df["user"] == user)
+        if not user_row.any():
+            st.error("Utilisateur introuvable.")
+            return
+        old_ok = (df.loc[user_row, "password"].iloc[0] == old_pwd)
+        if not old_ok:
+            st.error("Ancien mot de passe incorrect.")
+        elif not new_pwd1 or not new_pwd2:
+            st.error("Le nouveau mot de passe ne peut pas être vide.")
+        elif new_pwd1 != new_pwd2:
+            st.error("Les nouveaux mots de passe ne correspondent pas.")
+        elif new_pwd1 == old_pwd:
+            st.warning("Le nouveau mot de passe doit être différent de l'ancien.")
+        else:
+            df.loc[user_row, "password"] = new_pwd1
+            # Sauvegarde SÛRE
+            df.to_csv(db_path, index=False)
+            st.success("Votre mot de passe a été changé avec succès !")
+            st.session_state["logged"] = False
+            st.session_state.pop("user", None)
+            st.info("Veuillez vous reconnecter avec votre nouveau mot de passe.")
+            st.rerun()
     # Page d'accueil "welcome" une fois connecté
     col1, col2, col3 = st.columns([2, 5, 2])
     with col2:
