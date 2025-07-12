c42df42f28c5f5a3756c9a5b0eea5511a301f81c
TheNameLess001 <bounoirsaifeddine1@gmail.com>
2025-07-12 13:50:42 +0100

    Update 101_test.py

--- pages/101_test.py
+++ pages/101_test.py
@@ -1,18 +1,11 @@
-import streamlit as st
 from sqlalchemy import create_engine
 
-st.title("TEST CONNEXION À FPK_DASH")
-
-PG_USER = 'postgres'
-PG_PASS = 'Samoju123@'
-PG_HOST = '127.0.0.1'
-PG_DB   = 'FPK_DASH'
 PG_URL = "postgresql+psycopg2://postgres:Samoju123%40@127.0.0.1:5432/FPK_DASH"
 
 try:
     engine = create_engine(PG_URL)
     with engine.connect() as conn:
-        st.success("✅ Connexion réussie à la base FPK_DASH !")
-        st.write("Résultat du SELECT 1 :", conn.execute("SELECT 1").fetchone())
+        print("Connexion OK !")
+        print(conn.execute("SELECT 1").fetchone())
 except Exception as e:
-    st.error(f"❌ Erreur de connexion : {e}")
+    print("Erreur de connexion :", e)
