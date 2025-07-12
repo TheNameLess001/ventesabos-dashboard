78f9b5dcefaf9d2cbaa1bd1421c1154bf2428bf4
TheNameLess001 <bounoirsaifeddine1@gmail.com>
2025-07-12 13:02:24 +0100

    Update 101_test.py

--- pages/101_test.py
+++ pages/101_test.py
@@ -6,9 +6,13 @@ PG_HOST = 'localhost'
 PG_DB   = 'postgres'
 PG_URL = f"postgresql://{PG_USER}:{PG_PASS}@{PG_HOST}:5432/{PG_DB}"
 
+print("Début script")  # ← Ici
 try:
+    print("Avant create_engine")  # ← Ici
     engine = create_engine(PG_URL)
+    print("Après create_engine")   # ← Ici
     with engine.connect() as conn:
+        print("Connexion établie")  # ← Ici
         print(conn.execute("SELECT 1").fetchone())
     print("Connexion OK !")
 except Exception as e:
