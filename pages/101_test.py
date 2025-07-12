4a2c2d214ea5ea4feae0928af221c74451fe3c2e
TheNameLess001 <bounoirsaifeddine1@gmail.com>
2025-07-12 12:59:46 +0100

    Update 101_test.py

--- pages/101_test.py
+++ pages/101_test.py
@@ -3,13 +3,10 @@ from sqlalchemy import create_engine
 PG_USER = 'postgres'
 PG_PASS = 'Samoju123@'
 PG_HOST = 'localhost'
-PG_DB   = 'FPK_DASH'
+PG_DB   = 'postgres'   # <<< Remplace ici par 'postgres'
 PG_URL = f"postgresql://{PG_USER}:{PG_PASS}@{PG_HOST}:5432/{PG_DB}"
 
-try:
-    engine = create_engine(PG_URL)
-    with engine.connect() as conn:
-        print(conn.execute("SELECT 1").fetchone())
-    print("Connexion OK !")
-except Exception as e:
-    print("Erreur de connexion:", e)
+engine = create_engine(PG_URL)
+
+with engine.connect() as conn:
+    print(conn.execute("SELECT 1").fetchone())
