e9d4d5cd5aa36e9d17f4f4769ead0f1676a8cbd7
TheNameLess001 <bounoirsaifeddine1@gmail.com>
2025-07-12 12:48:55 +0100

    Update 101_test.py

--- pages/101_test.py
+++ pages/101_test.py
@@ -3,11 +3,10 @@ from sqlalchemy import create_engine
 PG_USER = 'postgres'
 PG_PASS = 'Samoju123@'
 PG_HOST = 'localhost'
-PG_DB   = 'fpk_dash'   # <<< adapte si besoin
-
+PG_DB   = 'fpk_dash'   # mets ici exactement ce qui est dans pgAdmin
 PG_URL = f"postgresql://{PG_USER}:{PG_PASS}@{PG_HOST}:5432/{PG_DB}"
 engine = create_engine(PG_URL)
 
 with engine.connect() as conn:
-    res = conn.execute("SELECT 1")
-    print(res.fetchone())
+    print(conn.execute("SELECT 1").fetchone())
+
