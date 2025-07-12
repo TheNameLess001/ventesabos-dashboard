31074ebf086f311552bc26102ec7e7d148a4f49c
TheNameLess001 <bounoirsaifeddine1@gmail.com>
2025-07-12 12:50:08 +0100

    Update 101_test.py

--- pages/101_test.py
+++ pages/101_test.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 PG_USER = 'postgres'
 PG_PASS = 'Samoju123@'
 PG_HOST = 'localhost'
-PG_DB   = 'fpk_dash'   # mets ici exactement ce qui est dans pgAdmin
+PG_DB   = 'FPK_DASH'   # mets ici exactement ce qui est dans pgAdmin
 PG_URL = f"postgresql://{PG_USER}:{PG_PASS}@{PG_HOST}:5432/{PG_DB}"
 engine = create_engine(PG_URL)
 
