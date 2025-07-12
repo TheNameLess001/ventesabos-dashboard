683dc0ec89072755158fb3d6053a4f4b3aa58840
TheNameLess001 <bounoirsaifeddine1@gmail.com>
2025-07-12 12:50:50 +0100

    Update 101_test.py

--- pages/101_test.py
+++ pages/101_test.py
@@ -3,10 +3,9 @@ from sqlalchemy import create_engine
 PG_USER = 'postgres'
 PG_PASS = 'Samoju123@'
 PG_HOST = 'localhost'
-PG_DB   = 'FPK_DASH'   # mets ici exactement ce qui est dans pgAdmin
+PG_DB   = 'FPK_DASH'     # <<< Mets FPK_DASH exactement, respecte la casse (majuscules/minuscules)
 PG_URL = f"postgresql://{PG_USER}:{PG_PASS}@{PG_HOST}:5432/{PG_DB}"
 engine = create_engine(PG_URL)
 
 with engine.connect() as conn:
     print(conn.execute("SELECT 1").fetchone())
-
